d604b69db449eeacecff6d47b93a77344df2e4a7
MadEldar <49936697+MadEldar@users.noreply.github.com>
2020-01-15 15:15:41 +0700

    Assignment 01

---
+++ assignment01.php
@@ -0,0 +1,221 @@
+<?php
+$products = [
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/12/31/637133775508815451_Vsmart-active-3-green-dd.png',
+        'offer' => 'Lì xì 400.000đ',
+        'name' => 'Vsmart Active 3 6GB-64GB',
+        'price' => 4990000,
+        'discountedFrom' => '',
+        'ratings' => 52,
+        'avgRatings' => 4.5,
+        'size' => '1080 x 2340 Pixels 6.39 inchs',
+        'camera' => '48 MP f/1.7 + 8MP f/2.2 + 2MP',
+        'battery' => '4.020 mAh',
+        'ram' => '6 GB',
+        'cpu' => 'MediaTek Helio P60 8 nhân',
+        'operatingSystem' => 'Android 9'
+    ],
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/12/30/637133110068362744_Vsmart-active-3-xanh-dd.png',
+        'offer' => 'Lì xì 300.000đ',
+        'name' => 'Vsmart Active 3 4GB-64GB',
+        'price' => 4490000,
+        'discountedFrom' => '',
+        'ratings' => 34,
+        'avgRatings' => 3.5,
+        'size' => '1080 x 2340 Pixels 6.39 inchs',
+        'camera' => '48 MP f/1.7 + 8MP f/2.2 + 2MP',
+        'battery' => '4.020 mAh',
+        'ram' => '4 GB',
+        'cpu' => 'MediaTek Helio P60 8 nhân',
+        'operatingSystem' => 'Android 9'
+    ],
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/11/6/637086359745271611_vsmart-live-6g-dd-1.png',
+        'offer' => 'Trả góp 0%',
+        'name' => 'Vsmart Live 6GB-64GB',
+        'price' => 3790000,
+        'discountedFrom' => 7790000,
+        'ratings' => 158,
+        'avgRatings' => 3.5,
+        'size' => '1080 x 2232 pixel 6.2 inchs',
+        'camera' => '48MP f/1.7 + 5MP f/1.9+8 MP',
+        'battery' => '4000mAh',
+        'ram' => '6 GB',
+        'cpu' => 'Qualcomm® Snapdragon™',
+        'operatingSystem' => 'Android 9'
+    ],
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/11/20/637098559835625732_vsmart-active1-plus-hong-dd.png',
+        'offer' => 'Lì xì 1.000.000đ',
+        'name' => 'Vsmart Active 1+',
+        'price' => 3590000,
+        'discountedFrom' => 4590000,
+        'ratings' => 216,
+        'avgRatings' => 3.5,
+        'size' => '1080 x 2280 Pixels 6.18 inches',
+        'camera' => '24.0 MP + 12.0 MP 20.0 MP',
+        'battery' => '3650 mAh',
+        'ram' => '6 GB',
+        'cpu' => 'Qualcomm Snapdragon 660',
+        'operatingSystem' => 'VOS (Based on Android 8.1)'
+    ],
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/9/18/637044121945221188_vsmart-joy-2-xd-dd.png',
+        'offer' => 'Lì xì 400.000đ',
+        'name' => 'Vsmart Joy 2+ 2GB-32GB',
+        'price' => 2190000,
+        'discountedFrom' => 2590000,
+        'ratings' => 52,
+        'avgRatings' => 3.5,
+        'size' => '720 x 1520 Pixels 6.2 inchs',
+        'camera' => '13MP f/2.0 + 5 MP f/2.4 8.0',
+        'battery' => '4500mAh',
+        'ram' => '2 GB',
+        'cpu' => 'Qualcomm® Snapdragon™',
+        'operatingSystem' => 'Android 9'
+    ],
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/9/3/637031200213155211_vsmart-star-dd-cam.png',
+        'offer' => '',
+        'name' => 'Vsmart Star 2GB-16GB',
+        'price' => 1390000,
+        'discountedFrom' => '',
+        'ratings' => 57,
+        'avgRatings' => 3.5,
+        'size' => '720 x 1520 Pixels 5.7 inchs',
+        'camera' => '8 MP f/2.0 + 2 MP f/2.4 5.0',
+        'battery' => '3000mAh',
+        'ram' => '2 GB',
+        'cpu' => 'Qualcomm® Snapdragon™',
+        'operatingSystem' => 'Android 9'
+    ],
+    [
+        'image' => 'https://images.fpt.shop/unsafe/fit-in/192x192/filters:quality(90):fill(white)/cdn.fptshop.com.vn/Uploads/Originals/2019/8/19/637018338168983020_637001058285288578_vsmart-bee-dd.png',
+        'offer' => '',
+        'name' => 'Vsmart BEE 1GB-16GB',
+        'price' => 990000,
+        'discountedFrom' => 1390000,
+        'ratings' => 53,
+        'avgRatings' => 3.5,
+        'size' => '720 x 1440 Pixels 5.45 inchs',
+        'camera' => '8.0 MP 5.0 MP',
+        'battery' => '2500 mAh',
+        'ram' => '1 GB',
+        'cpu' => 'MTK6739',
+        'operatingSystem' => 'Android 8.2 Go Edition Android 8'
+    ],
+];
+?>
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <style>
+        .product {
+            width: 25%;
+        }
+
+        .product > * {
+            padding: 5% 10% 0;
+        }
+
+        .product-image {
+            position: relative;
+        }
+
+        .product-image img {
+            width: 100%;
+        }
+
+        .product-image p {
+            position: absolute;
+            padding: 5px;
+            background-color: red;
+            color: white;
+            top: 5px;
+            right: 75px;
+        }
+
+        i {
+            color: orange;
+        }
+
+        .color-gray {
+            color: gray;
+        }
+    </style>
+    <title>Assignment 01</title>
+</head>
+<body>
+<div style='width: 80%;margin: 5% 10%;display: flex;flex-wrap: wrap;'>
+    <?php foreach ($products as $product) { ?>
+        <div class='product'>
+            <div class='product-image'>
+                <img src='<?php echo $product['image']; ?>' alt='<?php echo $product['name']; ?>'>
+                <?php if ($product['offer'] != '') { ?>
+                    <p><?php echo $product['offer']; ?></p>
+                <?php } ?>
+            </div>
+            <div class='product-main'>
+                <p><?php echo $product['name']; ?></p>
+                <b><?php echo number_format($product['price'], 0, ".", ","); ?></b>
+                <b class="color-gray">
+                    <?php if (gettype($product['discountedFrom']) != 'string') {
+                        echo number_format($product['discountedFrom'], 0, ".", ",");
+                    } ?>
+                </b>
+                <p>
+                    <span>
+                        <?php
+                        for ($i = 0; $i < 5; $i++) {
+                            if ($i < ($product['avgRatings'] - 1)) {
+                                echo '<i class=\'fas fa-star\'></i>';
+                            } else if ($i == ($product['avgRatings'] - 0.5)) {
+                                echo '<i class=\'fas fa-star-half-alt\'></i>';
+                            } else {
+                                echo '<i class=\'far fa-star\'></i>';
+                            }
+                        }
+                        ?>
+                    </span>
+                    <span class='color-gray'>
+                        <?php echo '(' . $product['ratings'] . ' đánh giá)' ?>
+                    </span>
+                </p>
+            </div>
+            <div class='product-details color-gray'>
+                <div>
+                    <b>Màn hình:</b>
+                    <span><?php echo $product['size']; ?> </span>
+                </div>
+                <div>
+                    <b>Camera:</b>
+                    <span><?php echo $product['camera']; ?> </span>
+                </div>
+                <div>
+                    <b>Pin:</b>
+                    <span><?php echo $product['battery']; ?> </span>
+                </div>
+                <div>
+                    <b>RAM:</b>
+                    <span><?php echo $product['ram']; ?> </span>
+                </div>
+                <div>
+                    <b>CPU:</b>
+                    <span><?php echo $product['cpu']; ?> </span>
+                </div>
+                <div>
+                    <b>HĐH:</b>
+                    <span><?php echo $product['operatingSystem']; ?> </span>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+</body>
+</html>
